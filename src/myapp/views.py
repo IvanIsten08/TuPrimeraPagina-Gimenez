bb618fe8353f9b366afd7642f693b651ea8a0228
Ivan Gimenez <ivanlgimenez@hotmail.com>
2025-03-02 16:43:29 -0300

    feat: parametros desde la url

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -7,3 +7,8 @@ def saludar(request):
 
 def saludar_con_etiqueta(request):
     return HttpResponse("<h1>hola desde Django con etiquetas!</h1>")
+
+def saludar_con_parametros(request, nombre: str, apellido: str):
+    nombre = nombre.capitalize() # Capitalizo el nombre
+    apellido = apellido.capitalize() # Capitalizo el apellido
+    return HttpResponse(f"hola {nombre} {apellido} desde Django con parametros!")
