750e2b1a263870c19ffc3d09854ee5bac643d3d3
Ivan Gimenez <ivanlgimenez@hotmail.com>
2025-03-02 16:34:35 -0300

    feat: respuesta simple

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def saludar(request):
+    return HttpResponse("hola desde Django!")
+
+def saludar_con_etiqueta(request):
+    return HttpResponse("<h1>hola desde Django con etiquetas!</h1>")
