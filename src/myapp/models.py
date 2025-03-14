e2f3304209c9ff3f3e6df02bb6e523ce065527e5
Ivan Gimenez <ivanlgimenez@hotmail.com>
2025-03-14 16:48:37 -0300

    feat: modelo CLiente creado

--- src/myapp/models.py
+++ src/myapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=50) # Todo lo que tenga Field quiere decir que es un tipo de datos, aqui indica que es de tipo Char y que tiene un maximo de 50 caracteres
+    apellido = models.CharField(max_length=50)
+    
